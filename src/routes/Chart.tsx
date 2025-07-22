537d0c1848cba3cac15135697e2d8d13100067c0
Attmoon <gus89580@naver.com>
2025-07-22 22:19:40 +0900

    Price chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,17 @@
-function Chart() {
-    return <h1>Chart</h1>
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "./api";
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery({
+    queryKey: ["ohlcv", coinId],
+    queryFn: () => fetchCoinHistory(coinId),
+    refetchInterval: 5000,
+  });
+  return <h1>Chart</h1>;
 }
 
 export default Chart;
